df71c3b2f76ee8a54395e209aca9d4cd2efdc438
Doctor-yoi <106463935+Doctor-yoi@users.noreply.github.com>
2024-03-19 09:18:53 +0000

    clean useless dependencies

--- mencoForWindows_winui3/Service/ServiceManager.cs
+++ mencoForWindows_winui3/Service/ServiceManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
+﻿using System.Net.Http;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
